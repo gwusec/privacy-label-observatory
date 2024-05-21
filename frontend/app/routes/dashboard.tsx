d03d55079f1517ead8b943fc922acf8a4121027f
Ilinca Hirtopanu <ilincah@gwmail.gwu.edu>
2024-05-21 23:34:03 +0000

    runs route.js and dashboard tweaks

--- frontend/app/routes/dashboard.tsx
+++ frontend/app/routes/dashboard.tsx
@@ -1,12 +1,15 @@
-import { LoremIpsum } from "react-lorem-ipsum";
-
+import React from "react";
+//tailwind
 export default function Index() {
   return (
-    <div id="main-text" >
-      <h1> Dashboard</h1>
-      <LoremIpsum
-        p={5}
-      />
+    <div id="main-text">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">Dashboard</h1>
+      <p className="text-lg text-white-700 mb-4">
+        Welcome to your dashboard. Here you can find an overview of your activities and manage your settings.
+      </p>
+      <p className="text-lg text-white-700">
+        Feel free to explore the features and customize your preferences. If you need any help, don't hesitate to reach out to our support team.
+      </p>
     </div>
   );
 }
