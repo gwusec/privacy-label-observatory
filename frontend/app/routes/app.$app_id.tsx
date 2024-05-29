b2ff93667536041c47de4d136e429fd5c417a167
Faris Jiwad <fjiwad@gwu.edu>
2024-05-29 18:04:52 +0000

    Sample timeline currently in the works

--- frontend/app/routes/app.$app_id.tsx
+++ frontend/app/routes/app.$app_id.tsx
@@ -1,6 +1,11 @@
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
+import noPhoto from "../resources/no_available_photo.jpg"
+
+//CSS and Component for the timeline
+import VerticalTimeline from "~/components/VerticalTimeline";
+
 
 export async function loader({params}:LoaderFunctionArgs){
     const q = params.app_id
@@ -10,20 +15,42 @@ export async function loader({params}:LoaderFunctionArgs){
     const app = await fetch(process.env.BACKEND_API + "getApp?id=" + q)
     const data = await app.json()
     console.log("hello")
-    console.log(json(data))
     return(json(data))
 };
+
 
 export default function App() {
     const data = useLoaderData<typeof loader>();
     console.log(data)
     var app_name = data[0]["app_name"]
     var app_id = data[0]["app_id"]
+    var image_url = data[0]["image_url"]
     //console.log(app_name)
+
     return(
-        <div className="text-center mx-2">
-            <h1>{app_name}</h1>
-            <p>{app_id}</p>
+        <div>
+            <div className="flex items-center">
+                {image_url == undefined ?
+                <img
+                    className="w-40 h-40 rounded-xl mr-4"
+                    src={noPhoto}
+                />  
+                :
+                
+                <img
+                    className="w-40 h-40 rounded-xl mr-4"
+                    src={image_url}
+
+                />  
+                }
+                <div className="m-2">
+                    <h1 className="text-xl font-bold">{app_name}</h1>
+                    <h2 className="text-sm text-gray-500">{app_id}</h2>
+                </div>
+            </div>
+            <div>
+                <VerticalTimeline />    
+            </div>
         </div>
     )
 }
