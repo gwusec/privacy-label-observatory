473bbde439990beed12f9254ca8988787e68c086
Faris Jiwad <fjiwad@gwu.edu>
2024-06-05 15:08:37 +0000

    Temp changes to timeline, still in development

--- frontend/app/routes/app.$app_id.tsx
+++ frontend/app/routes/app.$app_id.tsx
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from 'react';
+
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
@@ -8,37 +10,63 @@ import VerticalTimeline from "~/components/VerticalTimeline";
 
 
 export async function loader({params}:LoaderFunctionArgs){
+    console.log("running this loader")
     const q = params.app_id
+    console.log(q)
     if(q == undefined){
         return
     }
-    const app = await fetch(process.env.BACKEND_API + "getApp?id=" + q)
+    const app = await fetch(process.env.BACKEND_API + "fullApp?id=" + q)
     const data = await app.json()
     console.log("hello")
     return(json(data))
 };
 
 
+
+
 export default function App() {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [privDetails, setPrivDetails] = useState([]);
+
+    const handleClick = (event: any, index: number) => {
+        console.log("called", index)
+        setActiveIndex(index)
+    };
+
+    const updateParent = (index: number) => {
+        setActiveIndex(index)
+    }
+    
+    
     const data = useLoaderData<typeof loader>();
     console.log(data)
     var app_name = data[0]["app_name"]
     var app_id = data[0]["app_id"]
     var image_url = data[0]["image_url"]
-    //console.log(app_name)
+    var privacy_types = data[1]["privacy"]
+
+    useEffect(() => {
+        setPrivDetails(privacy_types[activeIndex]["privacy_types"]["privacyDetails"])
+        console.log("privDetails", privDetails)
+    })
+
+    const checkValueInDetails = (value) => {
+        
+    };
 
     return(
         <div>
             <div className="flex items-center">
                 {image_url == undefined ?
                 <img
-                    className="w-40 h-40 rounded-xl mr-4"
+                    className="w-40 h-40 rounded-xl m-4"
                     src={noPhoto}
                 />  
                 :
                 
                 <img
-                    className="w-40 h-40 rounded-xl mr-4"
+                    className="w-40 h-40 rounded-xl m-4"
                     src={image_url}
 
                 />  
@@ -48,8 +76,40 @@ export default function App() {
                     <h2 className="text-sm text-gray-500">{app_id}</h2>
                 </div>
             </div>
-            <div>
-                <VerticalTimeline />    
+            <div className="flex">
+                <div className="mt-4 bg-white p-4 rounded-lg shadow w-fit ">
+                    <VerticalTimeline privtypes={privacy_types} activeIndex={activeIndex} updateParent={updateParent} handleClick={handleClick}/>   
+                </div>
+                <div className='p-2 flex w-full'>
+
+                    {/* Need to come back to this since currently it's wrong */}
+                    <div className="m-4 bg-white rounded-lg shadow w-full text-center" id='duty'>
+                        <h3 className=" ${checkValueInDetails('DATA_USED_TO_TRACK_YOU') ? 'bg-green-200' : ''}">Date Used to Track You</h3> 
+                    </div>
+
+                    <div className="m-4 bg-white rounded-lg shadow w-full text-center" id='dly'>
+                        <h3 className=" ${checkValueInDetails('DATA_LINKED_TO_YOU') ? 'bg-green-200' : ''}">Date Linked to You</h3>
+                    </div>
+
+                    <div className="m-4 bg-white rounded-lg shadow w-full text-center" id='dnly'>
+                    {checkValueInDetails('DATA_NOT_LINKED_TO_YOU') ? 
+                        <h3 className="bg-green-200">Hello</h3> 
+                        :
+                        <h3>Data Not Linked to You</h3>}
+                    </div>
+                </div>
+                <div>
+                    {activeIndex !== null && activeIndex < privacy_types.length && (
+                        <div>
+                            
+                        <div className="bg-white p-4 rounded-lg shadow">
+                            <h3 className="text-lg font-bold">Privacy Details</h3>
+                            <pre>{JSON.stringify(privacy_types[activeIndex], null, 2)}</pre>
+                        </div>
+
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     )
