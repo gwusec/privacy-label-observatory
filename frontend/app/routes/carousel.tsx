5492e96f512cd084139c0d0f71c826df58607246
Ilinca Hirtopanu <ilincah@gwmail.gwu.edu>
2024-06-11 00:41:36 +0000

    draft carousel

--- frontend/app/routes/carousel.tsx
+++
@@ -1,9 +0,0 @@
-import Swiper from 'swiper';
-// import Swiper styles
-import 'swiper/css';
-
-const swiper = new Swiper('.swiper', {
-    modules: [Navigation, Pagination],
-    speed: 400,
-    spaceBetween: 100,
-  });
