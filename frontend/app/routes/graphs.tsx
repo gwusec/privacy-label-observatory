d1c09c5fa986b8b22d87b4001c8e77b8792f0ad6
Faris Jiwad <fjiwad@gwu.edu>
2024-07-30 15:38:05 +0000

    Just double checking to make sure Venn Diagram works

--- frontend/app/routes/graphs.tsx
+++ frontend/app/routes/graphs.tsx
@@ -15,7 +15,7 @@ export async function loader({params}: LoaderFunctionArgs){
 export default function Graphs(){
     const { state } = useNavigation()
     const data = useLoaderData<typeof loader>();
-    data["data"]
+    console.log(data["data"])
 
     return(
         <>
@@ -27,7 +27,6 @@ export default function Graphs(){
         
         <div className="h-screen p-2">
             <h1>Graphs</h1>
-            <iframe className="pointer-events-none w-full" src="http://localhost:5601/app/dashboards#/view/e35304c0-2f07-11ef-be6b-3f0232c42c87?embed=true&_g=(refreshInterval:(pause:!t,value:60000),time:(from:now-15m,to:now))&_a=()&hide-filter-bar=true" height="600" width="800"></iframe>
             <VennDiagram/>
         </div>
         }
