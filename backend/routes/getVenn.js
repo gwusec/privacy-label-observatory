6264f6241e90eeb6b67d8c9cb517d81bdf7fc4f2
Faris Jiwad <fjiwad@gwu.edu>
2024-07-30 16:45:18 +0000

    Created a way to get the total of an index. Going to use that to create another index

--- backend/routes/getVenn.js
+++ backend/routes/getVenn.js
@@ -1,9 +1,6 @@
 var express = require("express")
 var router = express.Router()
 
-var express = require("express")
-var router = express.Router()
-
 const client = require("./../client")
 
 router.get('/', async function(req, res){
