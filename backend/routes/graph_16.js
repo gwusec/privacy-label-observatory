77bb47258bbdde0de01951cb2b5e1ca54368e216
Faris Jiwad <fjiwad@gwu.edu>
2024-07-30 21:30:24 +0000

    Making axios call to pull more data, almost halfway done with graph 16 data, just need values of sub divisions

--- backend/routes/graph_16.js
+++ backend/routes/graph_16.js
@@ -1,92 +1,16 @@
 var express = require("express")
 var router = express.Router()
+const axios = require('axios');
 
 const client = require("./../client")
 
 router.get('/', async function(req, res){
 
     var dnc = {}
-
+    const response = await axios.get('http://localhost:9000/total?run=run_00003')
+    totals = response.data
     
-    await client.count({
-        "index": "run_00069",
-        "query": {
-            "bool": {
-              "must": [
-                {
-                    "term": {
-                    "privacylabels.privacyDetails.privacyTypes.keyword": {
-                      "value": "Data Not Collected"
-                    }
-                  }
-                }
-              ]
-            }
-          }
-    }).then(async (r) => {
-        dnc["Data Not Collected"] = r["count"]
-    })
-
-    await client.count({
-        "index": "run_00069",
-        "query": {
-            "bool": {
-              "must": [
-                {
-                    "term": {
-                    "privacylabels.privacyDetails.privacyTypes.keyword": {
-                      "value": "Data Linked to You"
-                    }
-                  }
-                }
-              ]
-            }
-          }
-    }).then(async (r) => {
-        dnc["linked"] = r["count"]
-    })
-
-    await client.count({
-        "index": "run_00069",
-        "query": {
-            "bool": {
-              "must": [
-                {
-                    "term": {
-                    "privacylabels.privacyDetails.privacyTypes.keyword": {
-                      "value": "Data Not Linked to You"
-                    }
-                  }
-                }
-              ]
-            }
-          }
-    }).then(async (r) => {
-        dnc["not_linked"] = r["count"]
-    })
-
-    await client.count({
-        "index": "run_00069",
-        "query": {
-            "bool": {
-              "must": [
-                {
-                    "term": {
-                    "privacylabels.privacyDetails.privacyTypes.keyword": {
-                      "value": "Data Used to Track You"
-                    }
-                  }
-                }
-              ]
-            }
-          }
-    }).then(async (r) => {
-        dnc["track"] = r["count"]
-    })
-
-    
-
-    res.json(dnc)
+    res.json("hello")
 })
 
 module.exports = router;
