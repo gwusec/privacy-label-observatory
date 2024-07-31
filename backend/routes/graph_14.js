33b0554b117a5d801bef22116a39b3389e57dcea
Faris Jiwad <fjiwad@gwu.edu>
2024-07-31 20:45:45 +0000

    graph_14 backend function completed

--- backend/routes/graph_14.js
+++ backend/routes/graph_14.js
@@ -1,70 +1,32 @@
 var express = require("express")
 var router = express.Router()
 const axios = require('axios');
-
 const client = require("./../client")
 
-//Helper function that rounds to two decimal places
-var roundUpto = function(number, upto){
-    return Number(number.toFixed(upto));
-}
-
 router.get('/', async function(req, res){
-
-    var dnc = {}
-    const totalRequest = await axios.get('http://localhost:8017/total?run=run_00069')
+    const totalRequest = await axios.get('http://localhost:8017/graph14Helper?index=total')
     totals = totalRequest.data
+    combinations = {}
+    combinations["totals"] = totals
 
-    percentages = {}
-    console.log(totals)
+    const dnc = await axios.get('http://localhost:8017/graph14Helper?index=dnc')
+    totals = dnc.data
+    combinations["dnc"] = totals
 
-    //This is for all percentages of data not collected
-    var request = await axios.get('http://localhost:8017/helper?name=dnc')
-    dnc_totals = request.data
+    const dnlty = await axios.get('http://localhost:8017/graph14Helper?index=dnlty')
+    totals = dnlty.data
+    combinations["dnlty"] = totals
 
-    for(const [key, value] of Object.entries(dnc_totals)){
-        console.log(key, value)
-        var total = roundUpto(((value/totals["Data Not Collected"]) * 100), 2)
-        percentages["dnc_" + key] = total
-    }
+    const dlty = await axios.get('http://localhost:8017/graph14Helper?index=dlty')
+    totals = dlty.data
+    combinations["dlty"] = totals
 
-    //This is for all percentages of data not linked to you
-    var request = await axios.get('http://localhost:8017/helper?name=dnlty')
-    dnlty_totals = request.data
-
-    for(const [key, value] of Object.entries(dnlty_totals)){
-        console.log(key, value)
-        var total = roundUpto(((value/totals["Data Not Linked to You"]) * 100), 2)
-        percentages["dnlty_" + key] = total
-    }
-
-    //This is for all percentages of data linked to you
-    var request = await axios.get('http://localhost:8017/helper?name=dlty')
-    dlty_totals = request.data
-
-    for(const [key, value] of Object.entries(dlty_totals)){
-        console.log(key, value)
-        var total = roundUpto(((value/totals["Data Linked to You"]) * 100), 2)
-        percentages["dlty_" + key] = total
-    }
-
-    //This is for all percentages of data used to track you
-    var request = await axios.get('http://localhost:8017/helper?name=duty')
-    duty_totals = request.data
-
-    for(const [key, value] of Object.entries(duty_totals)){
-        console.log(key, value)
-        var total = roundUpto(((value/totals["Data Used to Track You"]) * 100), 2)
-        percentages["duty_" + key] = total
-    }
+    const duty = await axios.get('http://localhost:8017/graph14Helper?index=duty')
+    totals = duty.data
+    combinations["duty"] = totals
 
 
-
-    
-
-   console.log(percentages)
-
-    res.json(percentages)
+    res.json(combinations)
 })
 
 module.exports = router;
