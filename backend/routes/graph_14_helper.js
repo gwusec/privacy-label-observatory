e416ce94ab434628f759ee08b15aaa6572eb9bd7
Faris Jiwad <fjiwad@gwu.edu>
2024-07-31 20:45:45 +0000

    graph_14 backend function completed

--- backend/routes/graph_14_helper.js
+++ backend/routes/graph_14_helper.js
@@ -3,22 +3,8 @@ var router = express.Router();
 const client = require("./../client");
 
 router.get('/', async function(req, res) {
-//   var value = req.query.name;
-//   console.log(value);
-
-  const years = [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
-
-  const mustQueries = years.map(year => ({
-    range: {
-      'metadata.release_date': {
-        gte: `${year}-01-01`,
-        lte: `${year}-12-31`
-      }
-    }
-  }));
-
-  console.log(mustQueries)
-  
+  var value = req.query.index;
+  console.log(value);
 
   if (value == "dnc") {
     value = "Data Not Collected";
@@ -30,11 +16,72 @@ router.get('/', async function(req, res) {
     value = "Data Used to Track You";
   }
   else{
-    throw new Error("Not a valid value, try again!")
+    value = "Total";
   }
 
-    res.json("hello")
-  
+  const years = [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
+  dnc = {}
+
+  const mustQueries = years.map(year => ({
+    range: {
+      'metadata.release_date': {
+        gte: `${year}-01-01`,
+        lte: `${year}-12-31`
+      }
+    }
+  }));
+
+  for(let i=0; i < mustQueries.length; i++){
+    try {
+        if(value != "Total"){
+
+
+            const response = await client.count({
+            body: {
+                query: {
+                bool: {
+                    must: [
+                    {
+                        term: {
+                        "privacylabels.privacyDetails.privacyTypes.keyword": {
+                            value: value
+                        }
+                        }
+                    },
+                    {
+                        ...mustQueries[i]
+                    }
+                    ]
+                }
+                }
+            }
+            });
+            dnc[years[i]] = response.count;
+            }
+            else{
+                const response = await client.count({
+                    body: {
+                        query: {
+                        bool: {
+                            must: [
+                            {
+                                ...mustQueries[i]
+                            }
+                            ]
+                        }
+                        }
+                    }
+                    });
+                    dnc[years[i]] = response.count;
+                    }
+            }
+        catch (error) {
+            console.error(error);
+            res.status(500).send(error.message);
+        }
+    }
+
+    res.json(dnc)
 });
 
 module.exports = router;
