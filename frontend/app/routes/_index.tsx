ba0d4111f122d51e5c8aec3dd565e83b75e6badc
Ilinca Hirtopanu <ilincah@gwmail.gwu.edu>
2024-08-08 16:39:08 +0000

    eliminated graphs/explore page

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,193 +1,97 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { useTheme } from 'next-themes';
-import { useNavigation } from '@remix-run/react';
-import { FaSpinner } from 'react-icons/fa';
+import { useNavigate } from "react-router-dom";
+// import 'shepherd.js/dist/css/shepherd.css';
+import { useTheme } from "next-themes";
+import Shepherd from 'shepherd.js';
 
-const Index: React.FC = () => {
-  const { state } = useNavigation();
+import { FaSpinner } from "react-icons/fa";
+import { useNavigation } from "@remix-run/react";
+import LineChart from '~/components/LineChart';
+import Ratios from '~/components/Ratios';
+import MatrixChart from '~/components/MatrixChart';
+import VennDiagram from "~/components/VennDiagram";
+import YearGraph from "~/components/YearGraph"
+
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import PercentageGraph from "~/components/PercentageGraph";
+
+import { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [{
+    title: "Dashboard",
+  }];
+};
+
+export async function loader({params}: LoaderFunctionArgs){
+  const venn = await fetch(process.env.BACKEND_API + "venn")
+  const vennDiagramData = await venn.json()
+  const percentage = await fetch(process.env.BACKEND_API + "graph16")
+  const percentageData = await percentage.json()
+  const dates = await fetch(process.env.BACKEND_API + "graph14")
+  const dateJson = await dates.json()
+
+  const response = await fetch('http://localhost:8017/longitude');
+  const longitude = await response.json();
+  const response2 = await fetch('http://localhost:8017/ratios');
+  const ratios = await response2.json();
+  const response3 = await fetch('http://localhost:8017/matrix');
+  const matrix = await response3.json();
+
+
+  return [vennDiagramData, percentageData, dateJson, longitude, ratios, matrix]
+}
+
+export default function Index() {
+  const navigate = useNavigate()
+  const { state } = useNavigation()
+    const data = useLoaderData<typeof loader>();
+    const vennDiagram = data[0]
+    const percentage = data[1]
+    const dates = data[2]
+    const longitude = data[3]
+    const ratios = data[4]
+    const matrix = data[5]
+
+  const goToApps = () => {
+    navigate("/search");
+  }
+
+  const goToGraphs = () => {
+    navigate("/dashboard");
+  }
+
+  const goToInfo = () => {
+    navigate("/documentation");
+  }
+
   const { theme } = useTheme();
-  const scrollContainerRef = useRef<HTMLDivElement>(null);
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const totalPages = 4; // Update this according to the actual number of pages
-
-  const scrollTo = (direction: 'prev' | 'next') => {
-    const container = scrollContainerRef.current;
-    if (!container) {
-      console.error('Scroll container not found');
-      return;
-    }
-
-    const scrollAmount = direction === 'prev' ? -container.offsetWidth : container.offsetWidth;
-    container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-
-    if (direction === 'prev' && currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    } else if (direction === 'next' && currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
   return (
     <>
-      {state === 'loading' ? 
+      {state === "loading" ?
         <div className="z-50 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-2/4 h-2/4">
           <FaSpinner className="animate-spin" size={72} />
         </div>
-      :
-        <div className={`${theme === 'dark' ? 'bg-dark-gradient' : 'bg-light-gradient'} min-h-screen flex`}>
+        :
+        <div className={`min-h-screen ${theme === 'dark' ? 'bg-dark' : 'bg-light'} overflow-hidden`}>
           <div id="main-text">
-            <div className="mb-14">
-              <h1 className={`text-4xl font-bold mb-16 text-center ${theme === 'dark' ? 'text-dred' : 'text-red'}`}>
-                GWUSEC Privacy Label Observatory Wiki
-              </h1>
-              <p className="text-xl text-white-700 mb-4 text-center">
-                After 2021, Apple Store required apps updating or being put on the app store for the first time to specify privacy labels.
-              </p>
-              <h2 className="text-xl text-white-700 mb-20 text-center">
-                We collected nearly weekly snapshots of the privacy labels of 1.6+ million apps over the span of a year.
+
+            <div className="mb-20">
+              <h1 className="text-4xl font-semibold mb-16 text-center">GWU SEC Privacy Label Observatory Dashboard</h1>
+              <h2 className="text-lg text-white-700 mb-4 text-center">
+                We collected nearly weekly snapshots of the privacy labels of 1.6+ million apps over the span of a year. Explore our database:
               </h2>
-              <h1 className={`text-4xl text-white-700 text-center font-semibold ${theme === 'dark' ? 'text-dred' : 'text-red'}`}>
-                Here is what we found:
-              </h1>
-            </div>
-            <div ref={scrollContainerRef} className="w-full snap-x snap-mandatory flex space-x-80 overflow-hidden mb-16">
-              <div className="snap-start flex-shrink-0 w-3/4 flex items-start justify-center">
-                <h2 className={`text-2xl text-white-600 mb-10 font-bold scroll space-x-16 pl-40 ${theme === 'dark' ? 'text-dred' : 'text-red'}`}>
-                  Voluntary Update vs. Means to an End:
-                </h2>
-                <div className="items-start">
-                  <p className="text-lg text-white-600 mb-8 max-w-md items-start text-justify space-x-4 pl-16">
-                    The increase from 2021 to 2022 is mainly in new apps which are being published, where the privacy label is just an obstacle to the goal of adding an app to the app store.
-                  </p>
-                  <p className="text-lg text-white-600 mb-8 max-w-md items-start text-justify space-x-4 pl-16">
-                    Our research found that existing apps that voluntarily updated their privacy labels -- without a version update -- included more details about their data collection practices.
-                  </p>
-                  <p className="text-lg text-white-600 mb-8 max-w-md items-start text-justify space-x-4 pl-16">
-                    50% of older apps that added a label with a version update simply stated they don’t collect any data.
-                  </p>
-                  <p className="text-lg text-white-600 mb-8 max-w-md text-justify font-semibold">
-                    This implies more truthfulness in the apps which voluntarily updated their labels.
-                  </p>
-                </div>
+              <div className="flex justify-center items-center space-x-4">
+                <button onClick={() => goToApps()} className={`px-4 py-1 text-lg font-semibold rounded-full border ${theme === 'dark' ? 'text-dred hover:text-red hover:bg-white' : 'text-red hover:text-white hover:bg-red border-black'}`}>the Apps</button>
+                <button onClick={() => goToGraphs()} className={`px-4 py-1 text-lg font-semibold rounded-full border ${theme === 'dark' ? 'text-dred hover:text-red hover:bg-white' : 'text-red hover:text-white hover:bg-red border-black'}`}>the Graphs</button>
+                <button onClick={() => goToInfo()} className={`px-4 py-1 text-lg font-semibold rounded-full border ${theme === 'dark' ? 'text-dred hover:text-red hover:bg-white' : 'text-red hover:text-white hover:bg-red border-black'}`}>the Research</button>
               </div>
-              <div className="snap-start flex-shrink-0 w-3/4 flex items-center justify-center space-x-32 pl-60">
-                <div className="flex flex-col items-end space-y-4 mb-5">
-                  <h2 className={`text-2xl text-white-600 font-bold text-justify items-start mb-4 ${theme === 'dark' ? 'text-dred' : 'text-red'}`}>
-                    You get what you do (or don't) pay for:
-                  </h2>
-                  <p className="text-lg text-white-700 text-justify max-w-lg">
-                    When comparing paid vs. free apps, more free apps report data collection and tracking than those you pay for, perhaps reflecting additional revenue streams from free apps in targeted advertising and/or selling user data.
-                  </p>
-                </div>
-                <div className="flex flex-col items-end space-y-4 pl-12">
-                  <h3 className={`text-2xl text-white-600 font-bold text-justify items-start mb-4 ${theme === 'dark' ? 'text-dred' : 'text-red'}`}>
-                    Data collection in popular free apps:
-                  </h3>
-                  <p className="text-lg text-white-700 text-justify max-w-lg">
-                    The large audience increases the surveillance surplus, which may make it harder for app sellers to resist collecting a wider range of data to increase profits. Therefore, popular apps reflect more data collection compared to less popular apps.
-                  </p>
-                </div>
               </div>
-              <div className="snap-start flex-shrink-0 flex items-center justify-center pl-20">
-                <h2 className={`text-2xl text-white-600 mb-10 font-bold space-x-16 mb-40 ml-20 ${theme === 'dark' ? 'text-dred' : 'text-red'}`}>
-                  Data collection may have no boundaries:
-                </h2>
-                <p className="text-lg text-white-700 mb-16 text-justify pl-16 max-w-md">
-                  Many apps with a 4+ or 9+ content rating report tracking data and would be available to children under the content rating guidelines, implying that they are tracking the data of children.
-                </p>
-              </div>
-              <div className="snap-start flex-shrink-0 w-3/4 flex items-center justify-start pl-20">
-                <div className="flex flex-col">
-                  <h2 className={`text-2xl text-white-600 mb-10 font-bold scroll text-justify ${theme === 'dark' ? 'text-dred' : 'text-red'}`}>
-                    Larger sized apps:
-                  </h2>
-                  <p className="text-lg text-white-700 mb-16 text-justify pl-16 max-w-md">
-                    According to the privacy labels, larger apps collect and track more user data. This may be due to the fact that apps with larger footprints contain additional software libraries for the purpose of collecting data.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="flex justify-center items-center mb-8">
-              <span className={`text-xl ${theme === 'dark' ? 'text-white-700' : 'text-gray-700'}`}>
-                {currentPage}/{totalPages}
-              </span>
-            </div>
-            <div className="mt-40">
-              <p className="text-lg text-white-700 mb-16 mt-32 text-center">
-                After 2021, Apple Store required apps updating or being put on the app store for the first time to specify privacy labels. The various choices made by developers of apps are reflected here, whether it be underreporting data or reporting well, and the reasons behind each.
-              </p>
-            </div>
-            <div className="mb-16 relative">
-              <h1 className={`text-3xl mb-8 text-white-700 text-center font-bold ${theme === 'dark' ? 'text-dred' : 'text-red'}`}>But what are privacy labels?</h1>
-              <p className="text-lg text-white-300 font-medium text-center mb-4">Privacy labels are basically nutrition labels, where the app must indicate what data is collected and used compactly.</p>
-
-              <a className={`text-md font-medium text-center mb-4 block relative ${theme === 'dark' ? 'text-dred hover:text-grey' : 'text-red hover:text-white'}`}>
-                Data Used to Track You
-                <span className="hover-text">Data collected may be used to track users across apps and websites owned by other companies, including sharing data with third-party advertising networks and data brokers.</span>
-              </a>
-
-              <a className={`text-md font-medium text-center mb-4 block relative ${theme === 'dark' ? 'text-dred hover:text-grey' : 'text-red hover:text-white'}`}>
-                Data Linked to You
-                <span className="hover-text">Data is collected and is linked to the user’s identity.</span>
-              </a>
-
-              <a className={`text-md font-medium text-center mb-4 block relative ${theme === 'dark' ? 'text-dred hover:text-grey' : 'text-red hover:text-white'}`}>
-                Data Not Linked to You
-                <span className="hover-text">Data is collected but is de-identified or anonymized and is therefore not linked to the user’s identity.</span>
-              </a>
-
-              <a className={`text-md font-medium text-center mb-32 block relative ${theme === 'dark' ? 'text-dred hover:text-grey' : 'text-red hover:text-white'}`}>
-                Data Not Collected
-                <span className="hover-text">When an app has a label with the Data Not Collected Privacy Type, it implies that it does not collect any data from the user, and therefore does not include other Privacy Types.</span>
-              </a>
-            </div>
-
-            <style jsx>{`
-              .hover-text {
-                visibility: hidden;
-                opacity: 0;
-                width: 300px;
-                background-color: #555;
-                color: #fff;
-                text-align: center;
-                border-radius: 6px;
-                padding: 15px 10px;
-                position: absolute;
-                z-index: 1;
-                bottom: 125%;
-                left: 50%;
-                margin-left: -150px;
-                transition: opacity 0.3s;
-              }
-
-              a:hover .hover-text {
-                visibility: visible;
-                opacity: 1;
-              }
-            `}</style>
 
           </div>
-          <button
-            className={`absolute left-0 top-3/4 transform -translate-y-1/2 text-gray-700 rounded-full p-4 shadow-md ${theme === 'dark' ? 'text-white bg-grey hover:text-red hover:bg-white' : 'text-red hover:text-white hover:bg-red'}`}
-            style={{ fontSize: '1.5rem', padding: '1.5rem' }}
-            onClick={() => scrollTo('prev')}
-          >
-            &lt;
-          </button>
-          <button
-            className={`absolute right-0 top-3/4 transform -translate-y-1/2 text-gray-700 rounded-full p-4 shadow-md ${theme === 'dark' ? 'text-white bg-grey hover:text-red hover:bg-white' : 'text-red hover:text-white hover:bg-red'}`}
-            style={{ fontSize: '1.5rem', padding: '1.5rem' }}
-            onClick={() => scrollTo('next')}
-          >
-            &gt;
-          </button>
         </div>
       }
     </>
   );
-};
-
-
-export default Index;
+}
 
--- frontend/app/routes/dashboard.tsx
+++ frontend/app/routes/dashboard.tsx
@@ -72,70 +72,61 @@ export default function Index() {
         <div className={`min-h-screen ${theme === 'dark' ? 'bg-dark' : 'bg-light'} overflow-hidden`}>
           <div id="main-text">
 
-            <div className="mb-20">
-              <h1 className="text-4xl font-semibold mb-16 text-center">GWU SEC Privacy Label Observatory Dashboard</h1>
-              <h2 className="text-lg text-white-700 mb-4 text-center">
-                We collected nearly weekly snapshots of the privacy labels of 1.6+ million apps over the span of a year. Explore our database:
-              </h2>
-              <div className="flex justify-center items-center space-x-4">
-                <button onClick={() => goToApps()} className={`px-4 py-1 text-lg font-semibold rounded-full border ${theme === 'dark' ? 'text-dred hover:text-red hover:bg-white' : 'text-red hover:text-white hover:bg-red border-black'}`}>the Apps</button>
-                {/* <button onClick={() => goToGraphs()} className={`px-4 py-1 text-lg font-semibold rounded-full border ${theme === 'dark' ? 'text-dred hover:text-red hover:bg-white' : 'text-red hover:text-white hover:bg-red border-black'}`}>the Graphs</button> */}
-              </div>
-
+            <div>
               <div style={{ width: '80%',  margin: '0 auto' }}>
-                    <div className="mb-10 mt-10">
+                    <div className="mb-20 mt-10">
                         <h1 className="text-center font-bold">Longitude Data Chart</h1>
                             <LineChart data={longitude} />
                         <h3>A longitudinal view over the year-long collection period of the total number of apps and the total number of apps with privacy labels (compliant apps). For comparison, we also display the four Privacy Types over the same period. Each data point represents a snapshot of the Apple App Store on that date.</h3>
                     </div>
-                    <div className="mb-10">
+                    <div className="mb-20">
                       <h1 className="text-center font-bold">Purpose Chart</h1>
-                      <div className="flex flex-row space-x-4 mt-10 mb-10">
+                      <div className="flex flex-row space-x-4 mt-10 mb-20">
                           <div className="flex flex-col items-center w-1/3">
-                              <h1 className="text-center mb-4">Data Used to Track You</h1>
+                              <h1 className="text-center">Data Used to Track You</h1>
                               <Ratios data={ratios.DATA_USED_TO_TRACK_YOU} />
                           </div>
                           <div className="flex flex-col items-center w-1/3">
-                              <h1 className="text-center mb-4">Data Linked to You</h1>
+                              <h1 className="text-center">Data Linked to You</h1>
                               <Ratios data={ratios.DATA_LINKED_TO_YOU} />
                           </div>
                           <div className="flex flex-col items-center w-1/3">
-                              <h1 className="text-center mb-4">Data Not Linked to You</h1>
+                              <h1 className="text-center">Data Not Linked to You</h1>
                               <Ratios data={ratios.DATA_NOT_LINKED_TO_YOU} />
                           </div>
                       </div>
                       <h3>The ratios of the six Purposes for the Data Used to Track You, Data Linked to You and Data Not Linked to You Privacy Types. The denominator is the number of apps in the specific Privacy Type.</h3>
                     </div>
-                    <div className="mb-10">
+                    <div className="mb-20">
                       <h1 className="text-center font-bold">Matrix Charts</h1>
-                      <div className="flex flex-row space-x-4 mt-10 mb-10">
-                          <div className="flex flex-col items-center w-1/2">
-                              <h1 className="text-center mb-4">Data Linked to You</h1>
-                              <MatrixChart data={matrix.DATA_LINKED_TO_YOU} />
-                          </div>
-                          <div className="flex flex-col items-center w-1/2">
-                              <h1 className="text-center mb-4">Data Not Linked to You</h1>
-                              <MatrixChart data={matrix.DATA_NOT_LINKED_TO_YOU} />
-                          </div>
-                      </div>
-                      <h3>The ratios of of Data Categories by the reported Purpose for the Data Linked to You and Data Not Linked
-to You Privacy Types.</h3>
+                        <div className="flex flex-row space-x-4">
+                            <div className="flex flex-col items-center w-1/2">
+                                <h1 className="text-center">Data Linked to You</h1>
+                                <MatrixChart data={matrix.DATA_LINKED_TO_YOU} />
+                            </div>
+                            <div className="flex flex-col items-center w-1/2">
+                                <h1 className="text-center">Data Not Linked to You</h1>
+                                <MatrixChart data={matrix.DATA_NOT_LINKED_TO_YOU} />
+                            </div>
+                        </div>
+                        <h4>The ratios of of Data Categories by the reported Purpose for the Data Linked to You (left) and Data Not Linked
+                            to You (right) Privacy Types.</h4>
                     </div>
-                    <div className="mb-10"> 
+                    <div className="mb-20"> 
                         <h1 className="text-center  font-bold" >Venn Diagram</h1>
                         <VennDiagram data={vennDiagram}/>
                         <h3 className="mt-5">A Venn diagram of the number of apps in each
 of the four Privacy Types. Data Not Collected is mutually
 exclusive to the other three Privacy Types</h3>
                     </div>
-                    <div className="mb-10">
+                    <div className="mb-20">
                       <h1 className="text-center font-bold" >Data Collected per Payment Method</h1>
                         <PercentageGraph data={percentage} />
                         <h3 className="mt-5 text-wrap">The ratios of app costs for each of the four Privacy Types.  Free apps are more likely than paid apps to collect data, including data used to track and
 linked to users.</h3>
                     </div>
 
-                    <div className="mb-10">
+                    <div className="mb-20">
                     <h1 className="text-center  font-bold" >Years vs Type of Apps</h1>
                         <YearGraph data={dates} />
                         <h3 className="">The number of apps released during a given year for each of the four Privacy Types. The pink bars show the total
@@ -150,3 +141,5 @@ number of apps with privacy labels released in that year. </h3>
     </>
   );
 }
+
+
