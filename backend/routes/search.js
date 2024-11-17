41353cb747fb0ede8624a2615fafc11ed9e462e9
Faris Jiwad <fjiwad@gwu.edu>
2024-11-17 19:52:36 +0000

    First big batch of changes involving theme fixes, API fixes, and page styling fixes

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -3,14 +3,14 @@ var router = express.Router()
 
 const client = require("./../client")
 
-router.get("/", function(req, res){
+router.get("/", function (req, res) {
     q = req.query.q;
     run = req.query.run;
     if (q == undefined) {
         res.json({ "Error": "Missing parameters" })
         return;
     }
-    if(run == undefined){
+    if (run == undefined) {
         run = "run_000*"
     }
 
@@ -18,23 +18,25 @@ router.get("/", function(req, res){
     client.search({
         "index": run,
         "query": {
-            "match": {
+            "match_phrase_prefix": {
                 "app_name": q
             }
         },
-        "size": 20
-    }).then((r) => {
-        var hits = []
-        for(i in r.hits.hits){
-            app_name = r.hits.hits[i]._source.app_name
-            app_id = r.hits.hits[i]._source.app_id
-            hits.push({
-                "app_name": app_name,
-                "app_id": app_id
-            }) 
+        "size": 20,
+        collapse: {
+            field: "app_id.keyword" // Use the keyword subfield for collapsing
         }
+    }).then((r) => {
+        const hits = r.hits.hits.map(hit => ({
+            app_name: hit._source.app_name,
+            app_id: hit._source.app_id
+        }));
+
         res.json(hits);
-    })
+    }).catch((error) => {
+        console.error("Error:", error);
+        res.status(500).json({ "Error": "Search failed" });
+    });
 
     return;
 })
