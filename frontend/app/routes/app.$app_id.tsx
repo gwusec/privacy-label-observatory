647c3f366df28a6b8659b49a49aac1d9bf4d0235
Ozzy Simpson <14060578+ozzy-simpson@users.noreply.github.com>
2025-07-09 20:40:06 +0000

    Refactor the app detail page - Create a component for the app data columns so that they match in style, are repeatable, and easily modifiable - Display data columns when no data is collected to make it clearer that **we** collected data on the app, but the app doesn't collect in that grouping - Fix issue that some apps not seen/without data collected in early runs would display as no data ever collected, thus hiding the timeline

--- frontend/app/routes/app.$app_id.tsx
+++ frontend/app/routes/app.$app_id.tsx
@@ -1,10 +1,8 @@
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import { json } from "@remix-run/node";
-import Timeline from '~/components/Timeline';
 import { MetaFunction } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
-import { useEffect } from "react";
+import AppDetail from "~/components/AppDetail";
 
 export const meta: MetaFunction<typeof loader> = ({ data }) => {
     if (!data || data.length === 0) {
@@ -45,15 +43,23 @@ export async function loader({ params }: LoaderFunctionArgs) {
 };
 
 export default function searchApp() {
-
     const data = useLoaderData<typeof loader>();
     const app = data[1];
     const dates = data[0];
-    console.log(app);
+
+    // Search for a non-empty privacy index (determines if the horizontal timeline should be displayed)
+    const firstNonEmptyPrivacyIndex = app[1].privacy?.findIndex(
+        (privacy: any) =>
+            privacy.privacy_types.privacyDetails.privacyTypes &&
+            privacy.privacy_types.privacyDetails.privacyTypes.length > 0 &&
+            privacy.privacy_types.privacyDetails.privacyTypes.some(
+                (type: any) => type.dataCategories.length > 0 || type.purposes.length > 0
+            )
+    );
 
     return (
         <div className={`items-start h-full`}>
-            <Timeline data={app} dates={dates} />
+            <AppDetail data={app} dates={dates} firstIndex={firstNonEmptyPrivacyIndex} />
         </div>
-    )
+    );
 }
