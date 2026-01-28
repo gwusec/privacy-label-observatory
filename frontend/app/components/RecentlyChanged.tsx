112ca6b6556e4fa87c503377c30e50fe11e41e88
danalz5 <danielalzeidan@gmail.com>
2026-01-28 16:02:23 -0600

    apps with recent changes date + icons fixed

--- frontend/app/components/RecentlyChanged.tsx
+++ frontend/app/components/RecentlyChanged.tsx
@@ -1,6 +1,6 @@
 import { Link } from '@remix-run/react';
 
-export default function RecentlyChanged({ cacheList }: { cacheList: Record<string, { app_id: string; app_name: string; image_url: string; }> }) {
+export default function RecentlyChanged({ cacheList }: { cacheList: Record<string, { app_id: string; app_name: string; image_url: string; latestRunDate: string }> }) {
     return (
         <div>
             <h1>Apps With Recent Changes</h1>
@@ -9,14 +9,14 @@ export default function RecentlyChanged({ cacheList }: { cacheList: Record<strin
                     <li key={app.app_id} className="border-b">
                         <Link 
                             to={`/app/${app.app_id}`} 
-                            className="flex items-center gap-4 p-2 hover:bg-gray-100 rounded-md transition duration-200"
-                        >
+                            className="flex items-center gap-4 p-2 hover:bg-gray-100 rounded-md transition duration-200">   
+                            <span className="w-20 shrink-0 text-gray-500 text-sm">{app.latestRunDate} (est.)</span>
                             <img 
                                 className="w-12 h-12 rounded-xl" 
                                 src={app.image_url} 
                                 alt={app.app_name} 
                             />
-                            <strong className="text-blue-600">{app.app_name}</strong> (ID: {app.app_id})
+                            <strong className="text-blue-600 ">{app.app_name}</strong> (ID: {app.app_id})
                         </Link>
                     </li>
                 ))}
