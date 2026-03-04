0e3c68dd62314a0767bef5a384b74f179b435eb3
Arwa Alsahdi <Aalsahdi@gwu.edu>
2026-03-04 17:07:00 -0500

    Update aiOverviewGen.js

--- backend/utilities/aiOverviewGen.js
+++ backend/utilities/aiOverviewGen.js
@@ -1,7 +1,6 @@
 import { GoogleGenAI  } from "@google/genai";
 
-// const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
-const ai = new GoogleGenAI({ apiKey: "abjsdhksdjksjdsldkls" });
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 export async function generatePrivacySummary(appName, privacyData) {
     try {
