4936a16ec4f35372a42bb88b283d010039977dc9
Arwa Alsahdi <Aalsahdi@gwu.edu>
2026-03-04 16:53:40 -0500

    random api key for testing

--- backend/utilities/aiOverviewGen.js
+++ backend/utilities/aiOverviewGen.js
@@ -1,6 +1,7 @@
 import { GoogleGenAI  } from "@google/genai";
 
-const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+// const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+const ai = new GoogleGenAI({ apiKey: "abjsdhksdjksjdsldkls" });
 
 export async function generatePrivacySummary(appName, privacyData) {
     try {
