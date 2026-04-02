51b3cf9b6999ac14be4b367b6b020c8944758d90
Arwa Alsahdi <Aalsahdi@gwu.edu>
2026-04-01 23:52:12 -0400

    update dates to include last uploaded 2 runs (121 and 122)

--- backend/Misc/dateCalculator.py
+++ backend/Misc/dateCalculator.py
@@ -3,7 +3,7 @@ import json
 
 # Starting date from the given dataset
 start_date = datetime(2021, 7, 23)
-num_runs = 120  # Total runs
+num_runs = 122 # Total runs
 
 # Generate the list of runs with a 7-day interval
 runs = [{"run_number": f"run_{i:05d}", "date": (start_date + timedelta(weeks=i-1)).strftime("%Y-%m-%d")} for i in range(1, num_runs + 1)]
